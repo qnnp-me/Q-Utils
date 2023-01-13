3af9b0d6306281173a80fb28aacaad1fa84ba0c9
qnnp-me <wujipeng@foxmail.com>
2023-01-13 19:35:24 +0800

    changes

--- dist/utils.js
+++ dist/utils.js
@@ -5,21 +5,21 @@
 var __assign   = (
                    this && this.__assign
                  ) || function () {
-  __assign = Object.assign || function (t) {
-    for (var s, i = 1, n = arguments.length; i < n; i++) {
-      s = arguments[i]
-      for (var p in s) {
-        if (Object.prototype.hasOwnProperty.call(s, p)) {
-          t[p] = s[p]
+    __assign = Object.assign || function (t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i]
+            for (var p in s) {
+                if (Object.prototype.hasOwnProperty.call(s, p)) {
+                    t[p] = s[p]
+                }
+            }
         }
-      }
+        return t
     }
-    return t
-  }
-  return __assign.apply(this, arguments)
+    return __assign.apply(this, arguments)
 }
 /// <reference path="index.ts" />
 var App        = require('./utils/App')
 var Request    = require('./utils/Request')
 var helpers    = require('./utils/helpers')
-module.exports = __assign(__assign(__assign({}, App), Request), helpers)
+module.exports = __assign(__assign(__assign({}, App), Request), helpers);
