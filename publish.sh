686978a1088598813151b24febcff8825fe8ed02
qnnp-me <wujipeng@foxmail.com>
2023-01-14 02:51:19 +0800

    publish version patch

--- publish.sh
+++ publish.sh
@@ -14,29 +14,30 @@ select yn in "Yes" "No"; do
       exit
     fi
     printf "==========test success\n"
-    printf "==========building\n"
+    printf "==========building...\n"
     php build.php
-    printf "==========git commit and push dev\n"
+    printf "==========git commit and push dev...\n"
     git add .
     git commit -a -m "publish version $1"
     git push origin dev --tags
-    printf "==========git checkout main and merge dev\n"
+    printf "==========git checkout main and merge dev...\n"
     git checkout main
     git merge -m "Merge branch 'dev' for publish version $1" dev
-    printf "==========git push main\n"
+    printf "==========git push main...\n"
     git push origin main --tags
     printf "==========change package version NO.\n"
     npm version $1
-    printf "==========publish package\n"
+    printf "==========publish package...\n"
     npm publish
-    printf "==========git push main\n"
+    printf "==========git push main...\n"
     git push origin main --tags
-    printf "==========git checkout dev\n"
+    printf "==========git checkout dev...\n"
     git checkout dev
+    printf "==========all done...\n"
     break
     ;;
   No)
-    printf 'Bye.'
+    printf 'Bye.\n'
     exit
     ;;
   esac
