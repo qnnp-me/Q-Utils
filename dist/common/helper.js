f3d7fce56afc3fa0663c89d89b8358e84298bc7d
qnnp-me <wujipeng@foxmail.com>
2023-01-13 11:52:48 +0800

    add License

--- dist/common/helper.js
+++ dist/common/helper.js
@@ -1,13 +1,10 @@
 /*
- * !
  * Copyright (c) 2023. qnnp <qnnp@qnnp.me>
- *
  */
 
 "use strict";
 /*
  * Copyright (c) 2023. qnnp <qnnp@qnnp.me>
- *
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getObjectType = void 0;
