9f5f5a3194c5146be4d3e8ca17c9de36db09103b
qnnp-me <wujipeng@foxmail.com>
2023-01-13 18:57:45 +0800

    优化导入、分割代码

--- types/main.d.ts
+++ types/main.d.ts
@@ -5,41 +5,41 @@
 /// <reference path="references.d.ts" />
 /// <reference types="miniprogram-api-typings" />
 declare const _default: {
-    getType: (target: any) => string;
-    REQUEST: (options: WxRequest, listen?: RequestListen) => Promise<WxResponse>;
-    OPTIONS: (url: string,
-      data?: object,
-      options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
-      listen?: RequestListen) => Promise<WxResponse>;
-    GET: (url: string,
-      data?: object,
-      options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
-      listen?: RequestListen) => Promise<WxResponse>;
-    HEAD: (url: string,
-      data?: object,
-      options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
-      listen?: RequestListen) => Promise<WxResponse>;
-    POST: (url: string,
-      data?: object,
-      options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
-      listen?: RequestListen) => Promise<WxResponse>;
-    PUT: (url: string,
-      data?: object,
-      options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
-      listen?: RequestListen) => Promise<WxResponse>;
-    DELETE: (url: string,
-      data?: object,
-      options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
-      listen?: RequestListen) => Promise<WxResponse>;
-    TRACE: (url: string,
-      data?: object,
-      options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
-      listen?: RequestListen) => Promise<WxResponse>;
-    CONNECT: (url: string,
-      data?: object,
-      options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
-      listen?: RequestListen) => Promise<WxResponse>;
-    app: IAppOption;
-    init: (initApp?: IAppOption) => void;
+  getType: (target: any) => string;
+  REQUEST: (options: WxRequest, listen?: RequestListen) => Promise<WxResponse>;
+  OPTIONS: (url: string,
+    data?: object,
+    options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
+    listen?: RequestListen) => Promise<WxResponse>;
+  GET: (url: string,
+    data?: object,
+    options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
+    listen?: RequestListen) => Promise<WxResponse>;
+  HEAD: (url: string,
+    data?: object,
+    options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
+    listen?: RequestListen) => Promise<WxResponse>;
+  POST: (url: string,
+    data?: object,
+    options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
+    listen?: RequestListen) => Promise<WxResponse>;
+  PUT: (url: string,
+    data?: object,
+    options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
+    listen?: RequestListen) => Promise<WxResponse>;
+  DELETE: (url: string,
+    data?: object,
+    options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
+    listen?: RequestListen) => Promise<WxResponse>;
+  TRACE: (url: string,
+    data?: object,
+    options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
+    listen?: RequestListen) => Promise<WxResponse>;
+  CONNECT: (url: string,
+    data?: object,
+    options?: RequestOption<string | WechatMiniprogram.IAnyObject | ArrayBuffer>,
+    listen?: RequestListen) => Promise<WxResponse>;
+  app: IAppOption;
+  init: (initApp?: IAppOption) => void;
 };
 export = _default;
